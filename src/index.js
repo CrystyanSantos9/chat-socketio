8878ec62b2e4c45167bb8bc3c49955a8960cc100
Crystyan Santos <cryssantos9@gmail.com>
2022-08-03 01:42:25 +0000

    adicionando socket.io

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ const app = express()
 const os = require('os')
 const RoomModel = require('./models/room')
 const session = require('express-session')
+//inicia socket io 
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+
 
 const mongoose = require('mongoose')
 const { application } = require('express')
+const path = require('path')
 const mongoString = process.env.MONGO_STRING || 'mongodb://172.18.0.2:32017/chat-socketio'
 
 const PORT = process.env.PORT || 3000
@@ -13,6 +19,11 @@ const HOST = process.env.HOST || os.hostname()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+
+app.set('views',  path.join(__dirname,'views'))
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use(session({
     secret:process.env.SESSION_SECRET || 'socketio', //variavel de ambiente que fica dentro de .env
@@ -23,10 +34,10 @@ app.use(session({
     }
 }))
 
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
+
 
 app.get('/', (req, res)=> res.render('home'))
+
 // Quando usuário clicar em entrar em / será feito o post em /
 //salva dados na sessao
 //redireciona para /room 
@@ -48,13 +59,23 @@ app.get('/room', (req, res)=>{
    }
 })
 
+//Colocamos o socket io para observar por novas conexões
+io.on('connection', socket => {
+    //on = observa a chegada de novos eventos
+    //quando o evento addRoom aparecer, pegamos o que veio nele através 
+    //da função 
+    socket.on('addRoom', roomName => {
+        console.log('addRoom', roomName)
+    })
+})
+
 mongoose
     .connect(mongoString, { useNewUrlParser: true, useUnifiedTopology: true } )
     .then(async ()=>{
         const connectionStateInformation = await mongoose.STATES
         const connectionState = connectionStateInformation.connected === 1 ? "Connected":"Something got wrong"
     
-        app.listen(3000, ()=>{
+        http.listen(3000, ()=>{
             console.log(`Application running on host ${HOST} on port ${PORT} and database is ${connectionState}`)
         })
     })
